e6dad293c6a32819d56b9a39fca5aff78a800f33
Admin <Admin@DESKTOP-9GEOOAP>
2025-04-20 14:26:14 +0530

    maven changes

--- src/test/java/utils/elementUtils.java
+++ src/test/java/utils/elementUtils.java
@@ -1,7 +1,17 @@
 package utils;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.time.Duration;
+
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class elementUtils {
 	
@@ -12,12 +22,52 @@ public class elementUtils {
 	}
 
 	public void clickElement(WebElement ele) {
-		
+		ele = explicitWait(ele, 10);
 		ele.click();
 	}
 	
 	public void enterText(WebElement et,String sk) {
+		et = explicitWait(et, 10);
 		et.sendKeys(sk);
 	}
+	
+	public WebElement explicitWait(WebElement element, long duration) {
+		
+		WebDriverWait wait = new WebDriverWait(edriver, Duration.ofSeconds(duration));
+		return wait.until(ExpectedConditions.elementToBeClickable(element));
+	}
+	
+	 public static void scrollToElement(WebDriver driver, WebElement element) {
+	        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+	        js.executeScript("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", element);
+	    }
+	 
+	 public static void uploadFileWithRobot(String filePath) {
+	        try {
+	            // Copy the file path to the clipboard
+	            StringSelection selection = new StringSelection(filePath);
+	            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection, null);
+
+	            // Initialize Robot
+	            Robot robot = new Robot();
+	            robot.setAutoDelay(500);
+
+	            // Press CTRL+V to paste the file path
+	            robot.keyPress(KeyEvent.VK_CONTROL);
+	            robot.keyPress(KeyEvent.VK_V);
+
+	            robot.keyRelease(KeyEvent.VK_V);
+	            robot.keyRelease(KeyEvent.VK_CONTROL);
+
+	            // Press Enter
+	            robot.keyPress(KeyEvent.VK_ENTER);
+	            robot.keyRelease(KeyEvent.VK_ENTER);
+
+	        } catch (AWTException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	
 
 }
